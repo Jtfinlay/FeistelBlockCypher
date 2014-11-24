ce895a09b11aacb29d7cccb84dce2d5c17664da1
James Finlay <jtfinlay@ualberta.ca>
2014-11-24 12:49:24 -0700

    Very very basic server/client implementation.

--- SocketClient.java
+++ SocketClient.java
@@ -1,4 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SocketClient {
+
+    private String _hostname;
+    private int _port;
+    Socket socketClient;
+
+    public SocketClient(String hostname, int port) {
+        _hostname = hostname;
+        _port = port;
+    }
+
+    public void connect() throws UnknownHostException, IOException {
+        System.out.println("Ateemption to connect to "+_hostname+".");
+        socketClient = new Socket(_hostname, _port);
+        System.out.println("Connection established");
+    }
+
+    public void readResponse() throws IOException {
+        String userInput;
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+
+        System.out.println("Response from server:");
+        while ((userInput = stdIn.readLine()) != null) {
+            System.out.println(userInput);
+        }
+    }
+
+    public static void main(String args[]) {
+        SocketClient client = new SocketClient("localhost", 9000);
+        try {
+            client.connect();
+            client.readResponse();
+        } catch (UnknownHostException e) {
+            System.err.println("Host unknown.");
+        } catch (IOException e) {
+            System.err.println("Cannot establish connection.");
+        }
+    }
 
 }
--- SocketServer.java
+++ SocketServer.java
@@ -1,4 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+/**
+ * Socket server
+ */
 public class SocketServer {
-  
+
+    private ServerSocket serverSocket;
+    private int port;
+
+    public SocketServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        System.out.println("Starting socket server at port: " + port);
+        serverSocket = new ServerSocket(port);
+
+        // Listen for clients. Block until connection
+
+        System.out.println("Waiting for clients");
+        Socket client = serverSocket.accept();
+
+        // Client connected!
+        sendWelcomeMessage(client);
+    }
+
+    private void sendWelcomeMessage(Socket client) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        writer.write("Hello. You are connected to a simple socket server.");
+        writer.flush();
+        writer.close();
+    }
+
+    /**
+     * Main driver for server
+     */
+    public static void main(String[] args) {
+        int portNumber = 9000;
+
+        try {
+            SocketServer socketServer = new SocketServer(portNumber);
+            socketServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
