2a8663df310c58f861bf07e793db3155be31d039
James Finlay <jtfinlay@ualberta.ca>
2014-11-24 12:36:34 -0700

    Empty classes & C libs

---
+++ lib_encryption.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include "Encryption.h"
+
+JNIEXPORT void JNICALL Java_Encryption_encrypt
+(JNIEnv *env, jobject object, long *v, long *k) {
+	/* TEA encryption algorithm */
+	unsigned long y = v[0], z=v[1], sum = 0;
+	unsigned long delta = 0x9e3779b9, n=32;
+
+	while (n-- > 0) {
+		sum += delta;
+		y += (z<<4) + k[0] ^ z + sum ^ (z>>5) + k[1];
+		z += (y<<4) + k[2] ^ y + sum ^ (y>>5) + k[3];
+	}
+
+	v[0] = y;
+	v[1] = z;
+}
